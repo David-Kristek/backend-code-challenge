57d1e1792a8ce8ca21cd67c0d7d042b083badf06
david.kristek <david.kristek05@gmail.com>
2022-09-23 21:48:17 +0200

    Virtual atributes - height, width

--- models/Pokemon.ts
+++ models/Pokemon.ts
@@ -2,9 +2,29 @@ import { Model } from "objection";
 import { Type } from "./Type";
 
 export default class Pokemon extends Model {
+  max_weight?: string;
+  min_weight?: string;
+  max_height?: string;
+  min_height?: string;
   static getTableName() {
     return "pokemon";
   }
+  static get virtualAttributes() {
+    return ["max_weight", "min_weight", "max_height", "min_height"];
+  }
+  weight() {
+    return {
+      maximum: this.max_weight,
+      minimum: this.min_weight,
+    };
+  }
+  height() {
+    return {
+      maximum: this.max_height,
+      minimum: this.min_height,
+    };
+  }
+
   static relationMappings = {
     types: {
       relation: Model.ManyToManyRelation,
@@ -20,17 +40,17 @@ export default class Pokemon extends Model {
       },
     },
     evolutions: {
-        relation: Model.ManyToManyRelation,
-        modelClass: Pokemon,
-        join: {
-          from: "pokemon.id",
-          through: {
-            // pokemon_evolutions is the join table.
-            from: "pokemon_evolutions.pokemon_id",
-            to: "pokemon_evolutions.evolution_id",
-          },
-          to: "pokemon.id",
+      relation: Model.ManyToManyRelation,
+      modelClass: Pokemon,
+      join: {
+        from: "pokemon.id",
+        through: {
+          // pokemon_evolutions is the join table.
+          from: "pokemon_evolutions.pokemon_id",
+          to: "pokemon_evolutions.evolution_id",
         },
+        to: "pokemon.id",
       },
+    },
   };
 }
